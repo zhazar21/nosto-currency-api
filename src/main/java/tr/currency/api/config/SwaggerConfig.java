c0e4831df133bb31caac4694dbdaf66fb6c7f7ae
zhazar21 <zhazar21@gmail.com>
2021-10-18 00:21:37 +0300

    initial commits

---
+++ src/main/java/tr/currency/api/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package tr.currency.api.config;
+
+import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket postsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("tr.currency.api"))
+				.paths(PathSelectors.any())
+				.build().apiInfo(apiInfo()).pathMapping("/")
+				.securitySchemes(Lists.newArrayList(apiKey()));
+	}
+
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Currency API")
+				.description("Currency API reference for developers")
+				.termsOfServiceUrl("http://currencyapp.com")
+				.licenseUrl("zhazar.info@gmail.com").version("1.0").build();
+	}
+
+	@Bean
+	public SecurityConfiguration securityInfo() {
+		return new SecurityConfiguration(null, null, null, null, "", ApiKeyVehicle.HEADER, "Authorization", "");
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "Authorization", "header");
+	}
+
+}
